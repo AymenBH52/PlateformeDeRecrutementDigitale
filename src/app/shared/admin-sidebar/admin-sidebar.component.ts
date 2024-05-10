7acdf9060d7fea188a5676d4587e865f33d34e40
Malek Chtioui <chtiouimalek001@gmail.com>
2024-05-10 17:25:56 +0100

    Sidebar changes

--- src/app/shared/admin-sidebar/admin-sidebar.component.ts
+++ src/app/shared/admin-sidebar/admin-sidebar.component.ts
@@ -3,8 +3,24 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-admin-sidebar',
   templateUrl: './admin-sidebar.component.html',
-  styleUrls: ['./admin-sidebar.component.scss']
+  styleUrls: ['./admin-sidebar.component.scss'],
 })
 export class AdminSidebarComponent {
+  isTestsMenuOpen = false;
+  isCategoriesMenuOpen = false;
+  isQuizzesMenuOpen = false;
 
+  toggleMenu(menuType: string) {
+    switch (menuType) {
+      case 'tests':
+        this.isTestsMenuOpen = !this.isTestsMenuOpen;
+        break;
+      case 'categories':
+        this.isCategoriesMenuOpen = !this.isCategoriesMenuOpen;
+        break;
+      case 'quizzes':
+        this.isQuizzesMenuOpen = !this.isQuizzesMenuOpen;
+        break;
+    }
+  }
 }
